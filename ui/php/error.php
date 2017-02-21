2a19364d47dc390c2d3ef03fe58575da83576983
George Karma <gkarma@ucsd.edu>
2017-02-21 09:22:43 -0800

    Forget this

--- ui/php/error.php
+++ ui/php/error.php
@@ -7,8 +7,5 @@ function error_out(){
   echo json_encode($q_result);
 }
 //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-$ar = array();
-array_push($ar, "hi");
-array_push($ar, "hello");
-echo json_encode($ar);
+
 ?>
