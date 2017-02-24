a7f1d9fe910e798a4a90fa9bd48f7a42afc9d6c1
George Karma <gkarma@ucsd.edu>
2017-02-23 22:28:42 -0800

    Dynamic parallax on log in

--- ui/javascript/displayUser.js
+++ ui/javascript/displayUser.js
@@ -7,34 +7,36 @@ $(document).ready(function(){
 
   // Callback handler that will be called on success
   request.done(function (response, textStatus, jqXHR){
-    var category_data = JSON.parse(response);
 
-    var out = "";
+    var c_data = JSON.parse(response);
 
-    out += "Category info for user: " + category_data["username"] + "\n\n";
+    var $panels = $('.panels');
+    var $navList = $('.nav-menu-list');
+    var numPanels = 0;
 
-    for(var c_id in category_data){
+    printData(c_data);
+
+    for(var c_id in c_data){
 
       if(c_id == "username") continue;
 
-      out += "c_id: " + c_id + "\n"
-      out += "c_name: " + category_data[c_id]["c_name"] + "\n";
+      numPanels = numPanels + 1;
 
-      for(var y_sub in category_data[c_id]["y_subs"]){
-        out += "  y_sub: " + category_data[c_id]["y_subs"][y_sub] + "\n";
+      $panels.append( '<div id="mainparallax' + numPanels + '" cid="' + c_id + '"cname="' + c_data[c_id]["c_name"] + '"class="parallax_main parallax_main_general"><div id="settingsButton' + numPanels + '" class="settings-button" onclick="generic_settings(' + numPanels + ')"><img src="CSS/img/settings-gear.jpg" height="20px" width="20px" /></div><div id="showButton' + numPanels + '" class="show-button" onclick="generic_show(' + numPanels + ')"><img src="CSS/img/show.jpeg" height="20px" width="20px" /></div><div id="parallaxSettings' + numPanels + '" class="parallax-settings-wrap"><div class="parallax-settings"></div></div></div>');
+      $navList.append('<li draggable="true" onclick="hideMenu()"><a href="#mainparallax' + numPanels + '">Untitled</a></li>');
+
+      //style each parallax div with .css
+
+      for(var y_sub in c_data[c_id]["y_subs"]){
       }
 
-      for(var r_sub in category_data[c_id]["r_subs"]){
-        out += "  r_sub: " + category_data[c_id]["y_subs"][r_sub] + "\n";
+      for(var r_sub in c_data[c_id]["r_subs"]){
       }
 
-      for(var p_sub in category_data[c_id]["p_subs"]){
-        out += "  p_sub: " + category_data[c_id]["y_subs"][p_sub] + "\n";
+      for(var p_sub in c_data[c_id]["p_subs"]){
       }
-      out += "\n";
     }
 
-    alert(out);
 
   });
 
@@ -43,3 +45,36 @@ $(document).ready(function(){
       alert("HTTPRequest: " + textStatus + " " + errorThrown);
   });
 });
+
+
+
+function printData(c_data){
+
+  var out = "";
+
+  out += "Category info for user: " + c_data["username"] + "\n\n";
+
+  for(var c_id in c_data){
+
+    if(c_id == "username") continue;
+
+    out += "c_id: " + c_id + "\n"
+    out += "c_name: " + c_data[c_id]["c_name"] + "\n";
+
+    for(var y_sub in c_data[c_id]["y_subs"]){
+      out += "  y_sub: " + c_data[c_id]["y_subs"][y_sub] + "\n";
+    }
+
+    for(var r_sub in c_data[c_id]["r_subs"]){
+      out += "  r_sub: " + c_data[c_id]["y_subs"][r_sub] + "\n";
+    }
+
+    for(var p_sub in c_data[c_id]["p_subs"]){
+      out += "  p_sub: " + c_data[c_id]["y_subs"][p_sub] + "\n";
+    }
+    out += "\n";
+  }
+
+  alert(out);
+
+}
