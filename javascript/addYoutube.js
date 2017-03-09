f81b39affb99211c58ec2116b0ed581376d9f69e
George Karma <gkarma@ucsd.edu>
2017-03-08 18:43:58 -0800

    my stuff

--- javascript/addYoutube.js
+++ javascript/addYoutube.js
@@ -1,5 +1,5 @@
 youtubeList = [
-  
+
     'https://www.youtube.com/embed/FhC9R9oCAVk',
     'https://www.youtube.com/embed/_Z-f3RSNUsQ',
     'https://www.youtube.com/embed/3rSer5Lqg5w',
@@ -7,37 +7,37 @@ youtubeList = [
     'https://www.youtube.com/embed/_Z-f3RSNUsQ',
     'https://www.youtube.com/embed/3rSer5Lqg5w',
     'https://www.youtube.com/embed/UXnJ95N0F8I'
-    
+
 ];
 
 function addList( list ){
-    
+
     for( i = 0; i < youtubeList.length; i++ ){
-        
+
         addYoutubeVideo( youtubeList[ i ], i );
-        
+
     }
-    
+
 } addList( youtubeList );
 
 function addYoutubeVideo( link, num ){
-    
+
     $('#mainparallax1-youtube').append('<li class="youtubeVideo' + num + '"><iframe class="youtubeVideoFrame' + num + '" width="300" height="200" src="' + link + '" frameborder="0" allowfullscreen></iframe></li>');
-    
+
     $('.youtubeVideo' + num).css( 'top', '10%' );
     $('.youtubeVideo' + num).css( 'left', ((num*20) + 10) + '%' );
-    
+
 }
 
 function adjustSize( size ){
-    
+
     alert( "yeah" );
-    
+
     for( i = 0; i < youtubeList.length; i++ ){
-        
+
         //$('.youtubeVideoFrame' + num).css( 'width', ((size*2) + 200) +'px' );
         //$('.youtubeVideoFrame' + num).css( 'height', ((size*2) + 100) +'px' );
-        
+
     }
-    
+
 }
--- javascript/displayUser.js
+++ javascript/displayUser.js
@@ -14,68 +14,7 @@ $(document).ready(function(){
 
     var $panels = $('.panels');
     var $navList = $('.nav-menu-list');
-    var numPanels = 3;
-
-    var settings = '<div class="parallax-settings">' +
-                          '<form class="settings-form">' +
-                              '<div class="settings-categoryName">' +
-                                  'Category:' +
-                                  '<br>' +
-                                  '<input type="text" id="categoryName' + numPanels + '" placeholder="Name of category" class="categoryName">' +
-                                  '<input type="submit" value="Submit" onclick="updateMenuName( ' + numPanels + ' )">' +
-                              '</div>' +
-                              '<div class="settings-categoryBackground">' +
-                                  '<br> Background Image:' +
-                                  '<br>' +
-                                  '<input type="file" id="categoryBackground' + numPanels + '" class="categoryBackgroundSelect">' +
-                                  '<input type="submit" value="Submit" onclick="updateBackground( ' + numPanels + ' )">' +
-                              '</div>' +
-                              '<div class="settings-categoryOrganized">' +
-                                  '<br> Organized:' +
-                                  '<br>' +
-                                  '<select id="categoryOrganized' + numPanels + '" name="organized" style="width: 180px;">' +
-                                      '<option value="a-z">' +
-                                          'a-z:A-Z' +
-                                      '</option>' +
-                                      '<option value="z-a">' +
-                                          'z-a:Z-A' +
-                                      '</option>' +
-                                  '</select>' +
-                              '</div>' +
-                          '</form>' +
-                          '<div class="panel-information">' +
-                              '<div class="panel-information-accounts">' +
-                                  '<br> Account:' +
-                                  '<br>' +
-                                  '<select id="categoryAccounts3" name="accounts" style="width: 50%;" class="categoryAccounts" onchange="fetchMedia(this.value, 3)">' +
-                                      '<option value="YouTube">' +
-                                          'Youtube' +
-                                      '</option>' +
-                                      '<option value="Pinterest">' +
-                                          'Pinterest' +
-                                      '</option>' +
-                                      '<option value="Reddit">' +
-                                          'Reddit' +
-                                      '</option>' +
-                                  '</select>' +
-                              '</div>' +
-                              '<div class="panel-information-subscriptions">' +
-                                  '<br> Subscriptions to Include:' +
-                                  '<br>' +
-                                  '<form class="categorySubscriptions">' +
-                                      '<input type="checkbox" name="subscription" value="Science"> Science' +
-                                      '<br>' +
-                                      '<input type="checkbox" name="subscription" value="League of Legends"> League of Legends' +
-                                      '<br>' +
-                                      '<input type="checkbox" name="subscription" value="Art History"> Art History' +
-                                      '<br>' +
-                                      '<input type="checkbox" name="subscription" value="Wild Turtle Footage"> Wild Turtle Footage' +
-                                      '<br>' +
-                                      '<input type="submit">' +
-                                  '</form>' +
-                              '</div>' +
-                      '</div>';
-
+    var numPanels = 0;
 
     for(var c_id in c_data){
 
@@ -83,37 +22,22 @@ $(document).ready(function(){
 
       numPanels = numPanels + 1;
 
-      $panels.append( '<div id="mainparallax' + numPanels + '" class="parallax_main parallax_main_general">\n
-                        <div id="settingsButton' + numPanels + '" class="settings-button" onclick="generic_settings(' + numPanels + ')">\n
-                            <img src="CSS/img/settings-gear.jpg" height="20px" width="20px" />\n
-                        </div>\n
-                        <div id="showButton' + numPanels + '" class="show-button" onclick="generic_show(' + numPanels + ')">\n
-                            <img src="CSS/img/show.jpeg" height="20px" width="20px" />\n
-                        </div>\n
-                        <div id="parallaxSettings' + numPanels + '" class="parallax-settings-wrap">' +
-                        settings +
-                        '</div>\n\
-                      </div>' );
+      createNewParallax(numPanels); //createNewParallax(numPanels, c_id);
 
-      /*$panels.append( '<div id="mainparallax' + numPanels + '" class="parallax_main parallax_main_general"><div id="settingsButton' + numPanels + '" class="settings-button" onclick="generic_settings(' + numPanels + ')"><img src="CSS/img/settings-gear.jpg" height="20px" width="20px" /></div><div id="showButton' + numPanels + '" class="show-button" onclick="generic_show(' + numPanels + ')"><img src="CSS/img/show.jpeg" height="20px" width="20px" /></div><div id="parallaxSettings' + numPanels + '" class="parallax-settings-wrap"><div class="parallax-settings"><form class="settings-form">Category:<br><input type="text" name="categoryName" placeholder="Name of category" class="categoryName"><br></form></div></div></div>');*/
+      var y_links = c_data[c_id]["y_links"];
 
-       $navList.append('<li draggable="true" onclick="hideMenu()"><a href="#mainparallax' + numPanels + '">Untitled</a></li>');
-
-
-       var y_links = c_data[c_id]["y_links"];
+      //embedd the videos
+      addList(y_links); //addList(y_links, numPanels)
 
     }
 
   }); //End of request.done
-
-
 
   request.fail(function (jqXHR, textStatus, errorThrown){
       alert("HTTPRequest: " + textStatus + " " + errorThrown);
   });
 
 }); //END OF $(document).ready
-
 
 
 function printData(c_data){
