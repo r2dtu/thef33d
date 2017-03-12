0ce41a6456ff345fd3b93d435447f76146ed0fbe
HAJohnny <redditjohnny@gmail.com>
2017-03-11 16:21:22 -0800

    Reddit Auth Prep

---
+++ javascript/reddit-integration.js
@@ -0,0 +1,93 @@
+$(document).ready(function(){
+    // TODO DELETE THIS
+    // var clientId = "V5gDAsfre7yTWg"
+    // var clientSecret = "1-BXkiGXxLuENy3tPpU1aRizwN4"
+    // if(typeof refresh_token !== 'undefined'){
+    // const reddit = newReddit(clientId, clientSecret, refresh_token);
+    // var subscribedPromise = getSubs(reddit);
+    // subscribedPromise.then(function(fulfilled){
+    //     console.log(fulfilled);
+    // });
+    //console.log(subscribed);
+    }
+});
+
+/*
+ * Returns true if user's reddit account is connected, false otherwise.
+ */
+function redditIsLinked(username){
+    var message = {"message" : "get_rtoken"}
+    // Request refresh token from database.
+    var request = $.ajax({
+        url: "php/redditAccount.php",
+        type: "POST",
+        data: message
+    });
+    request.done(function (response, textStatus, jqXHR){
+        console.log(response);
+        return true;
+    });
+    request.fail(function (jqXHR, textStatus, errorThrown){
+        console.log(error);
+        return false;
+    });
+}
+
+/*
+ * Redirects user to reddit authentication page.
+ */
+function linkReddit(){
+    location.href = "/php/reddit/index.php";
+}
+
+/*
+ * Unlinks reddit accounts by removing the refresh_token from the database.
+ * Returns true if successful.
+ * Takes a thef33d.me username.
+ */
+function unlinkReddit(username){
+    // Request to remove refresh token from database.
+    var request = $.ajax({
+        url: "php/reddit.php",
+        type: "POST",
+        data: {"message": "clearRefresh", "username": username}
+    });
+
+}
+
+/*
+ * Constructs and returns a reddit object.
+ * Requires: client id, secret, and refresh token.
+ */
+function newReddit(client_id, client_secret, refresh_token){
+    const reddit = new window.snoowrap({
+        userAgent: 'Reddit Functionality for thef33d.me by /u/teamfeed',
+        clientId: client_id,
+        clientSecret: client_secret,
+        refreshToken: refresh_token
+    });
+    return reddit;
+}
+
+/*
+ * Takes a reddit object. Needs to be authenticated.
+ * Returns a promise storing array of strings containing subscribed subreddits.
+ */
+function getSubs(reddit){
+  var subredditPromise = reddit.getSubscriptions();
+  var subredditNames = [];
+  var promise = new Promise(function(resolve, reject){
+      subredditPromise.then(function(subreddits){
+        for(var i = 0; i < subreddits.length; i++){
+          subredditNames[i] = subreddits[i].display_name;
+        }
+        console.log(subredditNames);
+        resolve(subredditNames);
+      })
+      .catch(function(error){
+          reject("Error fetching subreddits!" + error);
+      })
+  });
+  return promise;
+
+}
--- php/reddit/index.php
+++ php/reddit/index.php
@@ -1,4 +1,17 @@
 <?php
+session_start();
+include 'error.php';
+
+// Connect to database
+try{
+    $conn = new PDO("mysql:host=localhost;dbname=thefeed", root, WTF110lecture);
+}catch(PDOException $e){
+  error_out();
+}
+
+// Get f33d username
+$username = $_SESSION["username"];
+
 if (isset($_GET["error"]))
 {
     echo("<pre>OAuth Error: " . $_GET["error"]."\n");
@@ -8,18 +21,18 @@ if (isset($_GET["error"]))
 
 $authorizeUrl = 'https://ssl.reddit.com/api/v1/authorize';
 $accessTokenUrl = 'https://ssl.reddit.com/api/v1/access_token';
-$clientId = '8-kkjNXlTfpV0Q';
-$clientSecret = 'J6W5Y5UgCiJssMxapEGtsIX4Ebk';
-$userAgent = 'zhrong0725';
+$clientId = 'V5gDAsfre7yTWg';
+$clientSecret = '1-BXkiGXxLuENy3tPpU1aRizwN4';
+$userAgent = 'Reddit Functionality for thef33d.me by /u/teamfeed';
 
-$redirectUrl = "http://167.160.173.105/reddit/";
+$redirectUrl = "http://localhost/php/reddit/";
 
 require("Client.php");
 require("GrantType/IGrantType.php");
 require("GrantType/AuthorizationCode.php");
-echo "HelloO";
+//echo "HelloO";
 $client = new OAuth2\Client($clientId, $clientSecret, OAuth2\Client::AUTH_TYPE_AUTHORIZATION_BASIC);
-echo "hello";
+//echo "hello";
 $client->setCurlOption(CURLOPT_USERAGENT,$userAgent);
 
 if (!isset($_GET["code"])){
@@ -32,14 +45,30 @@ else
 {
     $params = array("code" => $_GET["code"], "redirect_uri" => $redirectUrl);
     $response = $client->getAccessToken($accessTokenUrl, "authorization_code", $params);
-
     $accessTokenResult = $response["result"];
-    $client->setAccessToken($accessTokenResult["access_token"]);
-    $client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
-    echo($accessTokenResult["access_token"]);
-    echo('</br>');
-    echo($accessTokenResult["refresh_token"]);
-    $response = $client->fetch("https://oauth.reddit.com/api/v1/me.json");
+    $rtoken = $accessToeknResult["refresh_token"];
+
+    // Store refresh token in database to be retrieved by javascript
+    try{
+        $statement = $conn->prepare("UPDATE accounts SET r_rtoken='$rtoken' WHERE username='$username'")->execute();
+    }
+    catch(PDOException $e){
+        error_out();
+    }
+
+    // TODO Store $accessTokenResult["refresh_token"] to database per user
+
+
+    //$client->setAccessToken($accessTokenResult["access_token"]);
+    //$client->setAccessTokenType(OAuth2\Client::ACCESS_TOKEN_BEARER);
+    //echo($accessTokenResult["access_token"]);
+    //echo('</br>');
+    //echo($accessTokenResult["refresh_token"]);
+    //$response = $client->fetch("https://oauth.reddit.com/api/v1/me.json");
+    //$_SESSION['reddit_refresh'] = $(accessTokenResult["refresh_token"]);
+
+    //header('Location: ../');
+
     //$response = $client->fetch("http://www.reddit.com/dev/api#GET_subreddits_mine_contributor");
 
     //echo('<strong>Response for fetch me.json:</strong><pre>');
@@ -50,7 +79,7 @@ else
 <!DOCTYPE html>
 <html lang="en">
     <head>
-    
+
     </head>
     <meta charset="UTF-8">
     <title>FEED REDDIT</title>
@@ -62,10 +91,10 @@ else
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js" type="text/javascript"></script>
     <script type="text/javascript">
-        var refresh = "<?= $accessTokenResult["refresh_token"] ?>";
+        var refresh_token = "<?= $accessTokenResult["refresh_token"] ?>";
     </script>
-    <script type="text/javascript" src="reddit.js">
+    <script type="text/javascript" src="../../javascript/reddit-integration.js">
     </script>
     <!--script src="jsoauth2/test/user.js" type="text/javascript"></script-->
-    
+
 </html>
