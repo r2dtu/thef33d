0e41f9e1334fea5f908403a0890c435c9caafc43
George Karma <gkarma@ucsd.edu>
2017-03-13 06:59:36 -0400

    Bunch of category settings

--- php/category.php
+++ php/category.php
@@ -12,8 +12,9 @@ $subs = $_POST["subs"];
 $c_img = $_POST["c_img"];
 $table = $_POST["table"];
 
+//USER IS TRYING TO UPDATE C_NAME OR CREATE NEW CATEGORY
 if($c_newname){
-  $q_result = $conn->query("SELECT c_id FROM categories WHERE username='$username' AND c_name='$c_newname'")->fetchColumn();
+  $q_result = $conn->query("SELECT c_id FROM categories WHERE username='$username' AND c_name='$c_newname'")->fetcColumn();
   if($q_result){
     $result["can_update_or_create"] = "no";
     echo json_encode($result);
@@ -25,11 +26,18 @@ if($message == "create"){
 
     $c_id = uniqid();
     $statement = $conn->prepare("INSERT INTO categories (c_id, c_name, username, img) VALUES ('$c_id', '$c_newname', '$username', '$c_img')")->execute();
+
+    if ($subs) {
+      $result = $conn->prepare("DELETE FROM $table WHERE c_id='$c_id'")->execute();
+      foreach ($subs as $sub_name => $sub_id) {
+        $result = $conn->prepare("INSERT INTO $table (c_id, sub_name, sub_id) VALUES ('$c_id', '$sub_name', '$sub_id')")->execute();
+      }
+    }
+
     $result["can_create"] = "yes";
     echo json_encode($result);
-    exit();
 
-} else if($message == "update"){
+}else if($message == "update"){
 
     if($c_newname){
 
@@ -48,6 +56,9 @@ if($message == "create"){
       }
     }
 
+    $result["can_update_or_create"] = "yes";
+    echo json_encode($result);
+
 } else if($message == "deleteCategory"){
 
     $q_result = $conn->prepare("DELETE FROM categories WHERE c_id='$c_id'")->execute();
@@ -55,9 +66,21 @@ if($message == "create"){
     $q_result = $conn->prepare("DELETE FROM p_subs WHERE c_id='$c_id'")->execute();
     $q_result = $conn->prepare("DELETE FROM r_subs WHERE c_id='$c_id'")->execute();
 
-}
+}else if($message == "subsInCat"){
 
-$result["can_update_or_create"] = "yes";
-echo json_encode($result);
+  $sub_names = $_POST['sub_names'];
+  $table = $_POST['table'];
+
+  $result = array();
+
+  foreach($sub_names as $sub_name){
+
+    $q_result = $conn->query("SELECT sub_id FROM '$table' WHERE sub_name='$sub_name'")->fetcColumn();
+    if(isset($q_result)){
+      array_push($result, $sub_name);
+    }
+  }
+  echo json_encode($result);
+}
 
 ?>
